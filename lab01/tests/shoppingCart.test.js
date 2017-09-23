1cb919458678c014acdc4c5124919fea1c7e2e78
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2017-09-23 12:09:10 +0800

    Lab01

--- lab01/tests/shoppingCart.test.js
+++ lab01/tests/shoppingCart.test.js
@@ -4,4 +4,38 @@ var chai = require('chai');
 chai.should();
 
 describe('ShoppingCart', function () {
+    // 初始化 nightmare
+    var url = "http://localhost:8080/src"
+    var nightmare;
+    before(() => {
+        nightmare = Nightmare({
+            show: true
+        });
+    });
+
+    // 設定測試結束後，結束 nightmare
+    after(async () => {
+        await nightmare.end();
+    });
+
+    // 撰寫自動化測試
+    it('VIP 會員, 購買 200 元商品 2 件, 結帳金額為 400 元', async () => {
+        // Arrange
+        var productId = 1;
+        var qty = 2;
+        var expected = 400;
+        var actual = 0;
+
+        // Act
+        var priceText = await nightmare.goto(url)
+            .viewport(1000, 760)
+            .insert(`div.product:nth-child(1) input[name=qty]`, false)
+            .insert(`div.product:nth-child(1) input[name=qty]`, qty)
+            .select('select[name=memberLevel]', 'VIP')
+            .evaluate(() => document.querySelector('#price').innerText);
+        actual = parseInt(priceText);
+
+        // Assert
+        actual.should.equal(expected);
+    });
 });
